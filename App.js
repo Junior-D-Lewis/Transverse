0d9d0c1ff00eb1ced4761fdd1afaccaf3f023ccc
Lionel <toglanlionel@gmail.com>
2022-04-16 18:27:09 +0200

    ajout tailwind

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`bg-red-500`}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello</Text>
       <StatusBar style="auto" />
