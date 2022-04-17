7e73be2dc4544240e16dadca5d29d92c4c32af24
Lionel <toglanlionel@gmail.com>
2022-04-17 12:04:57 +0200

    add bottom nav bar, new login page  version

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
 import tw from 'twrnc';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Register from './components/Register';
 import Login from './components/Login';
 
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View >
-      <Login/>
+    
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ tabBarShowLabel: false,headerShown: false}}>
+        <Tab.Screen name="Home" component={Login}/>
+        <Tab.Screen name="Settings" component={Register} />
+      </Tab.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
